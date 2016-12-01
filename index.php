537e6c99c654b5d2e1ac769fd05f14c7fbd5abf7
LauraJoy7175 <jayandlaura@sky.com>
2016-12-01 01:55:38 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Home</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+		<link href="https://fonts.googleapis.com/css?family=Yatra+One" rel="stylesheet">
+        <link href="style.css" rel="stylesheet">
+		<script src="script.js"></script>
+		<link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+	<main>
+    <div class="container-fluid">
+		<div class="row">
+		<div class="col-md-12">
+			<img id="nav-image" alt="Elljoys Logo" src="Logo/Logo1(bigger).jpg" />
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<ul>
+				<li><a href="index.php"><b>Home</b></a></li>
+				<li><a href="about.html"><b>About</b></a></li>
+				<li><a href="gallery.html"><b>Gallery</b></a></li>
+				<li><a href="prices.html"><b>Prices</b></a></li>
+				<li><a href="contact.html"><b>Contact</b></a></li>
+  				</li>
+			</ul>
+		</div>
+	</div>
+	<body>
+	<div class="row">
+		<div class="col-md-12">
+			<h2 class="home-h2"><b>Beautiful hand crafted pieces of jewellery created<br>to suit your style and budget.</b></h2>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-12">
+			<img class="home-image" alt="Home Images" src="Photos/homeImages.png" />
+		</div>
+	</div>
+	</body>
+	<div class="row">
+		<div class="col-md-12">
+			<a href="https://twitter.com/Elljoys106">
+				<i class="fa fa-twitter" style="font-size:48px;float:left;width: 33.3%;padding: 10px;" aria-hidden="true"></i>
+			</a>
+			<div class="footer">
+				<h4>@Elljoys Web Design by <a class="jake-link" href="https://jakejoy99.github.io/MyPortfolio/">Jake Joy</a></h4>
+			</div>
+			<a href="http://www.facebook.com/Elljoys/">
+			<i class="fa fa-facebook" style="font-size:48px; float:right;width: 33.3%;text-align: right;padding: 10px;" aria-hidden="true"></i>
+			</a>
+		</div>
+	</div>
+</div>
+</main>
+</html>
